641760746b29641567eb80d2bf669a07989e745d
Léo Meyer <leocantarella@gmail.com>
2025-08-18 21:44:16 -0300

    Mudando o codigo para uma API Restful

--- src/main/java/com/estudos/biblioteca/controller/LivroController.java
+++ src/main/java/com/estudos/biblioteca/controller/LivroController.java
@@ -2,19 +2,43 @@ package com.estudos.biblioteca.controller;
 
 import com.estudos.biblioteca.model.Livro;
 import com.estudos.biblioteca.repository.LivroRepository;
+import com.estudos.biblioteca.service.LivroService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/livros")
 public class LivroController {
 
-    private final LivroRepository livroRepository;
+    private final LivroService livroService;
 
-    public LivroController(LivroRepository livroRepository) {
-        this.livroRepository = livroRepository;
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
     }
 
+    @GetMapping
+    public List<Livro> listarTodos(){
+        return livroService.listarTodos();
+    }
+
+    @PostMapping
+    public Livro criar(@RequestBody Livro livro){
+        return livroService.salvar(livro);
+    }
+
+    @PostMapping("/{id}/emprestar")
+    public ResponseEntity<String> emprestar(@PathVariable Long id){
+        livroService.emprestar(id);
+        return ResponseEntity.ok("Livro emprestado com sucesso!");
+    }
+
+    @PostMapping("/{id}/devolver")
+    public ResponseEntity<String> devolver(@PathVariable Long id){
+        livroService.devolver(id);
+        return ResponseEntity.ok("Livro devolvido com sucesso!");
+    }
 }
